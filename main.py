cd632c8be4ab5ab28b3996a7bff5433b9304a9a5
Borton1 <53750557+Borton1@users.noreply.github.com>
2022-02-28 23:39:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
 from spiralPrint import spiralPrint
 print(spiralPrint('https://raw.githubusercontent.com/avito-tech/python-trainee-assignment/main/matrix.txt'))
-# t = ['-1','dfsd']
-# p = [int(i) for i in t]
-# for i in p:
-#     if(i < 0):
-#         print('shit')
-# print(p)
-# mynewlist = [s for s in t if s.isdigit()]
-# print(mynewlist)
-# for i in t:
-#     if (i.isdigit() == False):
-#         print()
